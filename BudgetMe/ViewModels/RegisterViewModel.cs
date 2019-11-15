7d57280b7356ea228ad82a07b9adc8605ca42803
DIEGO IVAN PEREZ MICHEL <diego.ivan.perez.michel@gmail.com>
2019-11-15 19:26:30 -0400

    adjust login and register forms design

--- BudgetMe/ViewModels/RegisterViewModel.cs
+++ BudgetMe/ViewModels/RegisterViewModel.cs
@@ -1,16 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.ComponentModel;
 
 namespace BudgetMe.ViewModels
 {
     public class RegisterViewModel
     {
+        [DisplayName("Usuario")]
         public string UserName { get; set; }
+        [DisplayName("Contraseña")]
         public string Password { get; set; }
+        [DisplayName("Nombre")]
         public string FirstName { get; set; }
+        [DisplayName("Apellido")]
         public string LastName { get; set; }
+        [DisplayName("Correo")]
         public string Email { get; set; }
     }
 }
